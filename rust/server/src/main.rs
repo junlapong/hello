bb000d1011ae8662d19a2252ac17aac6a8189b0e
Junlapong L <junlapong@gmail.com>
2021-08-20 07:21:41 +0700

    add java

--- rust/server/src/main.rs
+++ rust/server/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let mut server = Nickel::new();
 
     server.get("/", middleware!("Hello, World!"));
-    server.get("/pong", middleware!("pong"));
+    server.get("/ping", middleware!("pong\n"));
 
     server.listen("0.0.0.0:3000");
 }
